f52b0aad20e173220edc407de5df777c9b4ef821
Keaton <kmiller3@uccs.edu>
2026-05-18 23:44:34 -0600

    Grid slots can hold and swap items. New items can be created.

---
+++ Assets/Scripts/GridItem.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Grid Item")]
+public class GridItem : ScriptableObject
+{
+	public Sprite itemSprite;
+}
--- Assets/Scripts/GridSlot.cs
+++ Assets/Scripts/GridSlot.cs
@@ -1,9 +1,56 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// An individual grid slot in the crafting grid
 /// </summary>
 public class GridSlot : MonoBehaviour
 {
-    
+    private GridItem currentItem = null;
+
+    private Image itemImage;
+
+    private Button gridButton;
+
+    private void Awake()
+    {
+        itemImage = transform.GetChild(0).GetComponent<Image>();
+        gridButton = GetComponent<Button>();
+	}
+
+    public bool HasItem()
+    {
+        return currentItem != null;
+    }
+
+    public void SetItem(GridItem item)
+    {
+        currentItem = item;
+
+		// set the sprite
+		itemImage.enabled = true;
+		itemImage.sprite = currentItem.itemSprite;
+        if(itemImage.type == Image.Type.Simple)
+        {
+			itemImage.preserveAspect = true;
+        }
+
+        // enable the grid button
+        gridButton.enabled = true;
+
+	}
+
+    public void SwapItem(GridSlot slot, GridItem item)
+    {
+        slot.SetItem(currentItem);
+        SetItem(item);
+    }
+
+    public void RemoveItem()
+    {
+        currentItem = null;
+        itemImage.sprite = null;
+        itemImage.enabled = false;
+        gridButton.enabled = false;
+	}
 }
